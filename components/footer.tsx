1374541d27c39a19148c390d854cca57cbbf40ac
Saad Bin Khalid <ayyansaad46@gmail.com>
2025-11-20 13:53:45 +0500

    Fixed relative paths in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,35 +11,35 @@ export function Footer() {
     {
       title: "Company",
       links: [
-        { name: "About Us", href: "company-history" },
-        { name: "Board Of Directors", href: "board-of-directors" },
-        { name: "Innovation Center", href: "innovation-center" },
-        { name: "Sustainability", href: "sustainability" },
-        { name: "Careers", href: "careers" },
-        { name: "Contact Us", href: "contact-us/EdqZlRZsDem7RUy0PxJG" },
+        { name: "About Us", href: "/company-history" },
+        { name: "Board Of Directors", href: "/board-of-directors" },
+        { name: "Innovation Center", href: "/innovation-center" },
+        { name: "Sustainability", href: "/sustainability" },
+        { name: "Careers", href: "/careers" },
+        { name: "Contact Us", href: "/contact-us/EdqZlRZsDem7RUy0PxJG" },
       ],
     },
     {
       title: "Products",
       links: [
-        { name: "Agriculture / Farming", href: "products" },
-        { name: "Paints", href: "paints" },
-        { name: "Trading Chemicals", href: "trading-chemicals" },
+        { name: "Agriculture / Farming", href: "/products" },
+        { name: "Paints", href: "/paints" },
+        { name: "Trading Chemicals", href: "/trading-chemicals" },
       ],
     },
     {
       title: "Informative",
       links: [
-        { name: "Formulation Guides", href: "formulation-guides" },
-        { name: "News", href: "news" },
-        { name: "White Papers", href: "general-white-papers" },
+        { name: "Formulation Guides", href: "/formulation-guides" },
+        { name: "News", href: "/news" },
+        { name: "White Papers", href: "/general-white-papers" },
       ],
     },
     {
       title: "Legal",
       links: [
-        { name: "Terms Of Use", href: "terms-of-use" },
-        { name: "Privacy Policy", href: "privacy-policy" },
+        { name: "Terms Of Use", href: "/terms-of-use" },
+        { name: "Privacy Policy", href: "/privacy-policy" },
       ],
     },
   ];
