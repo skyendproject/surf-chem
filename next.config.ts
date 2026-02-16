4c1eafa2d0efeccd70c42e8978a625db9f082b5a
Saad Bin Khalid <102467366+saad0510@users.noreply.github.com>
2026-02-16 18:30:30 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,11 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["blob.v0.dev", "firebasestorage.googleapis.com", "www.surfchem.co.uk"],
+    domains: [
+      "blob.v0.dev",
+      "firebasestorage.googleapis.com",
+      "https://storage.googleapis.com/flutterflow-io-6f20.appspot.com/projects/surfactant-chemicals-admin-c4s3ys/assets",
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
