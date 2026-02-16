ddb9fb76cfba6ecd8f5461ef0b3b8709abec8a99
Saad Bin Khalid <ayyansaad46@gmail.com>
2026-02-16 14:59:22 +0400

    Remove margins around company history html

--- app/company-history/page.tsx
+++ app/company-history/page.tsx
@@ -19,7 +19,7 @@ export default function CompanyHistoryPage() {
   return (
     <div className="bg-white min-h-screen flex flex-col">
       <Navigation />
-      <main className="flex-1 container mx-auto px-4 py-8">
+      <main className="flex-1">
         {loading ? (
           <div className="text-center text-gray-500">Loading company history...</div>
         ) : (
